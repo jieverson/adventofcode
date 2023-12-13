6709d0425202a92a3ac33fd34e5c02aeaee3b369
Jiéverson Maissiat <jieverson@gmail.com>
2023-12-13 09:44:38 -0300

    day 7: nice (◕‿◕)

---
+++ 2023/day7/pt2/index.js
@@ -0,0 +1,56 @@
+const fs = require('node:fs')
+
+const input = fs.readFileSync('./input.txt', 'utf8')
+
+console.log(input)
+
+const hands = input.replace(/T/g, 'a')
+    .replace(/J/g, '1').replace(/Q/g, 'c')
+    .replace(/K/g, 'd').replace(/A/g, 'e')
+    .split('\n').map(l => {
+        const line = l.split(' ')
+        let value = parseInt(line[0], 16)
+
+        let cards = new Int32Array(16)
+        let jokers = 0
+        line[0].split('').forEach(c => {
+            if(c === '1')
+                jokers++
+            else
+                cards[parseInt(c, 16)]++
+        })
+        console.log(jokers)
+        console.log(line)
+        cards = cards.sort((a, b) => b - a)
+        
+        if(cards[0] + jokers === 5){
+            value += 0x60000000
+        }
+        else if(cards[0] + jokers === 4){
+            value += 0x50000000
+        }
+        else if(cards[0] + jokers === 3 && cards[1] === 2){
+            value += 0x40000000
+        }
+        else if(cards[0] + jokers === 3){
+            value += 0x30000000
+        }
+        else if(cards[0] === 2 && cards[1] === 2){
+            value += 0x20000000
+        }
+        else if(cards[0] + jokers === 2){
+            value += 0x10000000
+        }
+        
+        return {
+            card: line[0],
+            value: value,
+            bid: parseInt(line[1])
+        }
+    })
+
+const result = hands.sort((a, b) => b.value - a.value)
+    .reduce((t, c, i) => c.bid * (hands.length - i) + t, 0)
+
+
+console.log(result)
